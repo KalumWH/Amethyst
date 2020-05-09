e50f981774ef33e94650c6ed49bf1fb1796cb0e6
Iskawo <bradwrightdiscord@gmail.com>
2020-05-09 12:09:34 -0500

    fixed error in ping command, but this code no work

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require('discord.js');
 module.exports.run = async (client, message, args) => {
   message.channel.send(`Pinging...`).then(async (m) => {
       m.delete()
-      m.channel.send(`\`[HEARTBEAT]\` ${Math.round(client.ws.urmom.ping)}ms\n\`[RESPONSE]\` ${m.createdTimestamp - message.createdTimestamp}ms`)
+      m.channel.send(`\`[HEARTBEAT]\` ${Math.round(client.ws.ping)}ms\n\`[RESPONSE]\` ${m.createdTimestamp - message.createdTimestamp}ms`)
   });
 }
 module.exports.help = {
